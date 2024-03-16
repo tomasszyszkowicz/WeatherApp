193494ef38bda8994613bc55b36c981970b13b36
Tomáš Szyszkowicz <108410704+tomasszyszkowicz@users.noreply.github.com>
2024-03-16 19:56:01 +0100

    Authentication (#5)
    * added login/register views and templates
    
    * finished the authentication via user model
    
    * added comments to views.py

--- main/views.py
+++ main/views.py
@@ -1,30 +1,94 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def home_page(request):
     """
     Renders the home page.
 
-    Paramaters:
-        request: The HTTP request object.
+    Parameters:
+        request (HttpRequest): The HTTP request object.
 
     Returns:
-        The rendered home.html template.
+        HttpResponse: The rendered home.html template.
     """
     return render(request, "home.html")
 
 
+@login_required
 def stats_page(request):
     """
     Renders the stats page.
 
-    Paramaters:
-        request: The HTTP request object.
+    Parameters:
+        request (HttpRequest): The HTTP request object.
 
     Returns:
-        The rendered stats.html template with the location and date as context variables.
+        HttpResponse: The rendered stats.html template with the location and date as context variables.
     """
     location = request.GET.get("location")
     date = request.GET.get("date")
 
     return render(request, "stats.html", {"location": location, "date": date})
+
+
+def login_view(request):
+    """
+    Handles the login view.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered login.html template with the login form.
+    """
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+
+def logout_view(request):
+    """
+    Handles the logout view.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: Redirects to the login page.
+    """
+    logout(request)
+    return redirect("login")
+
+
+def register_view(request):
+    """
+    Handles the register view.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered register.html template with the registration form.
+    """
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect("home")  # Redirect to a home page
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "register.html", {"form": form})
