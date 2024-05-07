f50c38e927b2157f7c2f65c9628f114592ed00fa
tomas szyszkowicz <tomasszyszkowicz@email.cz>
2024-05-07 15:32:28 +0200

    added default locatoins when user gets registered

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import FavoriteLocations, RecentLocations
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, help_text='Required. Add a valid email address.')
@@ -14,4 +15,22 @@ class CustomUserCreationForm(UserCreationForm):
         user.email = self.cleaned_data['email']
         if commit:
             user.save()
+
+            FavoriteLocations.objects.create(
+                user=user,
+                location1 = "New York",
+                location2 = "Berlin",
+                location3 = "Tokyo"
+            )
+
+            RecentLocations.objects.create(
+                user=user,
+                location1 = "‎",
+                location2 = "‎",
+                location3 = "‎",
+                location4 = "‎",
+                location5 = "‎",
+                location6 = "‎"
+            )
+
         return user
--- main/views.py
+++ main/views.py
@@ -100,6 +100,7 @@ def register_view(request):
     return render(request, "register.html", {"form": form})
 
 
+@login_required
 def typescript_page(request):
     """
     Renders the TypeScript page.
